097784349123b2db9d246425b3dde4bc3fe8e89d
xiaoshan5733 <wengqianshan5733@163.com>
2020-03-22 00:29:23 +0800

    resolution

--- lib.js
+++ lib.js
@@ -8,12 +8,13 @@ const del = require('del');
  * options.settings ffmpeg配置，参考: https://github.com/damianociarla/node-ffmpeg#videofnextractframetojpgdestinationfolder-settings-callback
  * 默认输出5张图片
  * options.dirname 指定输出目录，默认 ./files
+ * options.subdir: 二级目录名， 注意：要保证每个文件有一个唯一的二级目录，所以一般情况下不建议设置此项
  * @output {object}
  * {
  *   error: 错误信息，当存在错误信息时表示执行失败
  *   files: 返回文件列表
  *   dirname: 目录名，本次操作返回的文件目录
- *   subdir: 二级目录名， 注意：要保证每个文件有一个唯一的二级目录，所以一般情况下不建议设置此项
+ *   resolution: 分辨率
  * }
  */
 module.exports = async function videoExtracter (input, options = {}) {
@@ -31,9 +32,11 @@ module.exports = async function videoExtracter (input, options = {}) {
   let result = {};
   try {
     const video = await new ffmpeg(input);
+    const { resolution } = video.metadata.video;
     const files = await video.fnExtractFrameToJPG(dirname, settings);
     result.files = files;
     result.dirname = dirname;
+    result.resolution = resolution;
   } catch (e) {
     result.error = e.message;
     // 提取失败，删除预置目录
